185f0cad2d8cd252f24ade3c9f3809d81a04b35a
Ivar Saris <ivarsaris@gmail.com>
2020-01-25 20:29:35 +0000

    added user registration functionality

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,43 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
 class UserLoginform(forms.Form):
     """form for user to log in"""
+
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+class UserRegistrationForm(UserCreationForm):
+    """form to register a new user"""
+
+    password1 = forms.CharField(label="Password",
+                                widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Confirm password",
+                                widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'password1', 'password2']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exclude(username=username):
+            raise forms.ValidationError('Account with this Email address already exists.')
+        return email
+
+    def clean_password(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+
+        if not password1:
+            raise ValidationError("Please enter a password.")
+
+        if not password2:
+            raise ValidationError("Please confirm your password.")
+
+        if password1 != password2:
+            raise ValidationError("Passwords must match.")
+
+        return password2
--- accounts/views.py
+++ accounts/views.py
@@ -1,19 +1,24 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
-from accounts.forms import UserLoginform
+from django.contrib.auth.decorators import login_required
+from accounts.forms import UserLoginform, UserRegistrationForm
 
 def index(request):
     """return index.html"""
+
     return render(request, 'index.html')
 
+@login_required
 def logout(request):
     """log out user"""
+
     auth.logout(request)
     messages.success(request, "You have successfully logged out.")
     return redirect(reverse('index'))
 
 def login(request):
     """return login page"""
+
     if request.user.is_authenticated:
         return redirect(reverse('index'))
 
@@ -30,9 +35,19 @@ def login(request):
                 return redirect(reverse('index'))
 
             else:
-                login_form.add_error(None, "Username or password is incorrect.")
+                login_form.add_error(None, "the username or password provided is incorrect.")
 
     else:
         login_form = UserLoginform()
 
-    return render(request, 'login.html', {'login_form': login_form})
+    return render(request, 
+                'login.html', 
+                {'login_form': login_form})
+
+def register(request):
+    """return registration page"""
+
+    registration_form = UserRegistrationForm()
+    return render(request,
+                 'register.html', 
+                {'registration_form': registration_form})
