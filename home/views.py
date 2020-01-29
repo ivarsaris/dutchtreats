30a2c2e4bfe5a656f4daefd7217fb6e40cd02c77
Ivar Saris <ivarsaris@gmail.com>
2020-01-29 10:12:06 +0000

    created products app

--- home/views.py
+++ home/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
 def about(request):
     """display about page"""
 
     return render(request, 'about.html')
+
 
 def contact(request):
     """display contact page"""
