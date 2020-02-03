7ec2e36e5817dfda640ad5051489c904e5241d78
Ivar Saris <ivarsaris@gmail.com>
2020-02-03 09:48:13 +0000

    created checkout functionalities and urls

--- dutchtreats/urls.py
+++ dutchtreats/urls.py
@@ -21,6 +21,7 @@ from products import urls as products_urls
 from cart import urls as cart_urls
 from search import urls as search_urls
 from home import urls as home_urls
+from checkout import urls as checkout_urls
 from products.views import all_products
 from django.views import static
 from .settings import MEDIA_ROOT
@@ -32,6 +33,7 @@ urlpatterns = [
     url(r'^products/', include(products_urls)),
     url(r'^cart/', include(cart_urls)),
     url(r'^search/', include(search_urls)),
+    url(r'^checkout/', include(checkout_urls)),
     url(r'^home/', include(home_urls)),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT}),
 ]
