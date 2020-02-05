d1dca2d094a6fa85fb02d7b76df1f48f959ca7e0
Ivar Saris <ivarsaris@gmail.com>
2020-02-05 10:02:20 +0000

    fixed error in allowed hosts

--- dutchtreats/settings.py
+++ dutchtreats/settings.py
@@ -27,7 +27,10 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://dutchtreats.herokuapp.com/']
+ALLOWED_HOSTS = [
+    'dutchtreats.herokuapp.com/',
+    'localhost',
+]
 
 
 # Application definition
