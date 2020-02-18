4706d37a86265943daf0931bc9c2ffe715883ff1
Ivar Saris <ivarsaris@gmail.com>
2020-02-18 08:07:52 +0000

    wrote product review models

--- products/models.py
+++ products/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-
-# Create your models here.
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Product(models.Model):
+    """Product models, add product to the database"""
     name = models.CharField(max_length=254, default='')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
@@ -11,3 +12,14 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Review(models.Model):
+    """model to write review for a product"""
+    post = models.ForeignKey('products.Product', related_name='reviews')
+    user = models.ForeignKey(User, related_name='reviews', null=False, default=1, on_delete=models.SET_DEFAULT)
+    content = models.TextField()
+    review_date = models.DateTimeField(blank=True, null=True, default=timezone.now)
+
+    def __str__(self):
+        return self.content
