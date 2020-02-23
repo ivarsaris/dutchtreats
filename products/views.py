cc36829a9c9ea6e9cbc9a67254be7de7f9431520
Ivar Saris <ivarsaris@gmail.com>
2020-02-23 10:35:19 +0000

    created review views

--- products/views.py
+++ products/views.py
@@ -18,7 +18,8 @@ def product_detail(request, pk):
     it on product.html. Or return error 404 if product not found
     """
     product = get_object_or_404(Product, pk=pk)
-    return render(request, "product.html", {'product': product})
+    form = ReviewForm(request.POST)
+    return render(request, "product.html", {'product': product, 'form': form})
 
 def new_review(request, pk):
     """
@@ -27,7 +28,9 @@ def new_review(request, pk):
     """
     if not request.user.is_authenticated:
         # redirect user to login page if not authenticated
+        messages.warning(request, "You have to be logged in to post a review.")
         return redirect('login')
+
     else:
         reviews = Review.objects.all()
         product = get_object_or_404(Product, pk=pk)
@@ -36,11 +39,14 @@ def new_review(request, pk):
             form = ReviewForm(request.POST)
             if form.is_valid():
                 review = form.save(commit=False)
-                review.owner = request.user
-                review.product = product
-                return redirect('product', pk=post.pk)
-            else:
-                form = ReviewForm()
+                review.user = request.user
+                review.post = product
+                review.save()
+
+                messages.success(request, "Thanks for your review, it was saved successfully.")
+                return redirect('product', pk=product.pk)
+            
+        form = ReviewForm()
     
     return render(request, 'product.html', {'form': form, 'products': products, 'reviews': reviews, 'product': product})
 
@@ -59,18 +65,19 @@ def edit_review(request, pk):
                 return redirect('product', review.post.id)
         else:
             form = ReviewForm(instance=review)
-    else: return HttpResponseForbidden()
+    else:
+        return HttpResponseForbidden()
 
     return render(request, 'product.html', {'form': form, 'products': products, 'reviews': reviews, 'post': review.post})
 
 
 def delete_review(request, pk):
     """allow user to delete review"""
-    id = request. POST['review_id']
-    pk = request.POST['product_id']
+    # id = request.POST['review_id']  - From form related code.
+    # pk = request.POST['product_id'] - From form related code.
     product = get_object_or_404(Product, pk=pk)
-    review = get_object_or_404(Review, id=id)
-    if (request.user.is_authenticated and request.user == review.owner or request.user.is_superuser):
+    review = get_object_or_404(Review, pk=pk)
+    if (request.user.is_authenticated and request.user == review.user or request.user.is_superuser):
         if request.method == 'POST':
             try:
                 review.delete()
@@ -78,7 +85,9 @@ def delete_review(request, pk):
 
             except review.DoesNotExist:
                 messages.warning(request, "You can't delete this comment.")
-    
+
     else:
+        print("failed on checking user")
         return HttpResponseForbidden()
-    return redirect('product')
+
+    return render(request, 'index.html')
