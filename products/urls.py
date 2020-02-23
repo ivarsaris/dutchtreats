26a99f54ed90680cc4136fd87bbd5ff615fc6e0f
Ivar Saris <ivarsaris@gmail.com>
2020-02-23 10:36:01 +0000

    created review urls

--- products/urls.py
+++ products/urls.py
@@ -6,7 +6,7 @@ from .views import (all_products, product_detail, new_review,
 urlpatterns = [
     url(r'^$', all_products, name='products'),
     url(r'^(?P<pk>\d+)/$', product_detail, name='product'),
-    url(r'^<pk>/detail/review', new_review, name='new_review'),
+    url(r'^(?P<pk>\d+)/detail/review', new_review, name='new_review'),
     url(r'^review/<pk>/edit', edit_review, name='edit_review'),
-    url(r'^delete/review', delete_review, name='delete_review'),
+    url(r'^delete/review/(?P<pk>\d+)', delete_review, name='delete_review'),
 ]
