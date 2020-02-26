df7c9294f901d091d3be8caf901362e94692e4fb
Ivar Saris <ivarsaris@gmail.com>
2020-02-26 22:23:58 +0000

    added comments to cart files

--- cart/views.py
+++ cart/views.py
@@ -2,15 +2,15 @@ from django.shortcuts import render, reverse, redirect
 from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
 
-# Create your views here.
 def view_cart(request):
     """render the cart contents page"""
     return render(request, 'cart.html')
 
+"""add specific amount of a product to the cart"""
 def add_to_cart(request, id):
-    """add specific amount of a product to the cart"""
     if not request.user.is_authenticated:
-        # redirect user to login page if not authenticated
+
+        """"redirect user to login page if not authenticated""""
         messages.warning(request, "You have to be logged in to add products to your cart.")
         return redirect('login')
     
