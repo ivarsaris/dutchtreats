ce0b6fc16ec06ca505568102c47caaec12e81475
Ivar Saris <ivarsaris@gmail.com>
2020-02-26 22:48:19 +0000

    added comments to search files

--- search/views.py
+++ search/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from products.models import Product
 
 
-# Create your views here.
+"""allow user to search for a certain product in the products page"""
 def do_search(request):
     products = Product.objects.filter(name__icontains=request.GET['q'])
     return render(request, "products.html", {"products": products})
