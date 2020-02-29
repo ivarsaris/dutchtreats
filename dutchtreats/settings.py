bbd728c5d77cdaad93064c8a80267301892f4a08
Ivar Saris <ivarsaris@gmail.com>
2020-02-29 12:07:49 +0000

    removed import env and set DEBUG to False in setting for deployment

--- dutchtreats/settings.py
+++ dutchtreats/settings.py
@@ -1,5 +1,4 @@
 import os
-import env
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -13,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'dutchtreats.herokuapp.com',
