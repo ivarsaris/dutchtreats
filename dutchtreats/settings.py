c2fbdabd240b5adb9d5314a4aa55bdabda6ab539
Ivar Saris <ivarsaris@gmail.com>
2020-04-14 00:25:54 +0000

    changed setting to work in development

--- dutchtreats/settings.py
+++ dutchtreats/settings.py
@@ -134,8 +134,8 @@ AWS_S3_OBJECT_PARAMETERS = {
 
 AWS_STORAGE_BUCKET_NAME = 'dutchtreats'
 AWS_S3_REGION_NAME = 'eu-west-3'
-AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_ACCESS_KEY_ID = "AKIA2VMXHQU6REJT325K"
+AWS_SECRET_ACCESS_KEY = "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"
 
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
@@ -153,8 +153,8 @@ DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 
-STRIPE_PUBLISHABLE = os.getenv("STRIPE_PUBLISHABLE")
-STRIPE_SECRET = os.getenv("STRIPE_SECRET")
+STRIPE_PUBLISHABLE = "pk_test_dHmJzECpJESEUquRaJT1Nnoj00JnDCmqZq"
+STRIPE_SECRET = "sk_test_4JCL2hTnJiOlUD1nhqqdbmav00Ovjn2HVW"
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
