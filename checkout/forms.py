0f6ed3529213033b4fbbccb1512fed97b87e0b11
Ivar Saris <ivarsaris@gmail.com>
2020-04-17 02:29:30 +0000

    added credit card errors in case user enters wrong info

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,16 +1,33 @@
 from django import forms
 from .models import Order
 
+# class MakePaymentForm(forms.Form):
+
+#     MONTH_CHOICES = [(i, i) for i in range(1, 12)]
+#     YEAR_CHOICES = [(i, i) for i in range(2020, 2039)]
+
+#     credit_card_number = forms.CharField(label='Credit card number', required=False)
+#     cvv = forms.CharField(label='Security code (CVV)', required=False)
+#     expiry_month = forms.ChoiceField(label='Expiry month', choices=MONTH_CHOICES, required=False)
+#     expiry_year = forms.ChoiceField(label='Expiry year', choices=YEAR_CHOICES, required=False)
+#     stripe_id = forms.CharField(widget=forms.HiddenInput)
+
 """get all neccessary information for payment with stripe credit card"""
 class MakePaymentForm(forms.Form):
 
     MONTH_CHOICES = [(i, i) for i in range(1, 12)]
     YEAR_CHOICES = [(i, i) for i in range(2020, 2039)]
 
-    credit_card_number = forms.CharField(label='Credit card number', required=False)
-    cvv = forms.CharField(label='Security code (CVV)', required=False)
-    expiry_month = forms.ChoiceField(label='Expiry month', choices=MONTH_CHOICES, required=False)
-    expiry_year = forms.ChoiceField(label='Expiry year', choices=YEAR_CHOICES, required=False)
+    credit_card_number = forms.CharField(label='Credit card number. Stripe credit card test number is 4242424242424242',
+                                         required=False)
+    cvv = forms.CharField(label='Security code (CVV)',
+                          required=False)
+    expiry_month = forms.ChoiceField(label='Expiry month',
+                                     choices=MONTH_CHOICES,
+                                     required=False)
+    expiry_year = forms.ChoiceField(label='Expiry year',
+                                    choices=YEAR_CHOICES,
+                                    required=False)
     stripe_id = forms.CharField(widget=forms.HiddenInput)
 
 class OrderForm(forms.ModelForm):
