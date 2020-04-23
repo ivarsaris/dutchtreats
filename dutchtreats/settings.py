5a15371c1dd9332138ab6a2fbd8c6f8a74c323ad
Ivar Saris <ivarsaris@gmail.com>
2020-04-23 02:11:37 +0000

    changed settings for deployment

--- dutchtreats/settings.py
+++ dutchtreats/settings.py
@@ -1,7 +1,5 @@
 import os
-
 import dj_database_url
-import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -14,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'dutchtreats.herokuapp.com',
